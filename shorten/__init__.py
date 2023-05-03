7abd50cf96407f547d03a16a803154a6d3cf3dd4
Destinier <Destinier.kunal34@gmail.com>
2023-05-03 15:32:50 +0530

    added csrf protection

--- shorten/__init__.py
+++ shorten/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
+from flask_wtf import CSRFProtect
 app = Flask(__name__,instance_relative_config=True)
 app.config.from_pyfile("app_configs.py")
 
 from shorten.web.views import web
 app.register_blueprint(web)
-
+csrf = CSRFProtect(app)
 from shorten.web.models import db
 db.init_app(app)
 with app.app_context():
